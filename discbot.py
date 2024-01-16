2e1138ede367c682b9bddae117ad03f6e28b6f45
RhysI <jrhysi11@gmail.com>
2024-01-16 14:08:22 -0500

    changed perms for exiting bot, added random commands & tiktok embed

--- discbot.py
+++ discbot.py
@@ -1,10 +1,11 @@
 import interactions
 from interactions import listen
 import os
+import random
 
 BOT_TOKEN = os.getenv('TOKEN')
 
-bot = interactions.Client(intents=interactions.Intents.DEFAULT | interactions.Intents.MESSAGE_CONTENT, token=BOT_TOKEN)
+bot = interactions.Client(intents=interactions.Intents.DEFAULT | interactions.Intents.MESSAGE_CONTENT, token=BOT_TOKEN, delete_unused_application_cmds=True)
 print ("BOT Online")
 
 @interactions.slash_command(
@@ -12,6 +13,7 @@ print ("BOT Online")
     description="quits the bot",
     default_member_permissions=interactions.Permissions.ADMINISTRATOR,
 )
+@interactions.check(interactions.is_owner())
 async def exit_command_run(ctx: interactions.SlashContext):
     await ctx.send(ctx.author.mention + " quit the bot, Goodbye!" + (" (" + ctx.bot.owner.mention + " bot is down)"))
     await bot.stop()
@@ -63,6 +65,9 @@ async def fix_embed(event):
     elif "//x.com/" in fixed:
         fixed = fixed.replace("x", "fixvx")
         await event.message.reply(fixed, allowed_mentions=interactions.AllowedMentions(replied_user=False), silent=True)
+    elif "//www.tiktok.com/" in fixed:
+        fixed = fixed.replace("tiktok", "tiktxk")
+        await event.message.reply(fixed, allowed_mentions=interactions.AllowedMentions(replied_user=False), silent=True)
 
 @interactions.slash_command(
     name="returnstring",
@@ -77,6 +82,33 @@ async def fix_embed(event):
 )
 async def return_command(ctx: interactions.SlashContext, text: str):
     await ctx.send(f"You sent the string '{text}'!")
+
+
+@interactions.slash_command(
+    name="number",
+    description="Picks random number between values"
+)
+@interactions.slash_option(
+    name="high_num",
+    description="Upper bound for generator",
+    required=True,
+    opt_type=interactions.OptionType.INTEGER,
+)
+@interactions.slash_option(
+    name="low_num",
+    description="Lower bound for generator, 0 by default",
+    opt_type=interactions.OptionType.INTEGER,
+)
+async def num_gen(ctx: interactions.SlashContext, high_num: int, low_num: int = 0):
+    await ctx.send(random.randint(low_num, high_num), silent=True)
+
+@interactions.slash_command(
+    name="coinflip",
+    description="Flips a coin",
+)
+async def coin_flip(ctx: interactions.SlashContext):
+    coin = random.choice(["Heads", "Tails"])
+    await ctx.send(coin, silent=True)
 
 
 bot.start()
